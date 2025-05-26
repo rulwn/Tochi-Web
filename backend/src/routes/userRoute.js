c881d9b95275d715285b994e89d1dbb418fc0434
LuiiiDev <luisesc1210@gmail.com>
2025-05-25 21:09:35 -0600

    parte del merge que on se subio

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -1,11 +1,23 @@
 import express from 'express';
 import multer from 'multer';
 import userController from '../controllers/userController.js';
+import authMiddleware from "../middlewares/authMiddleware.js";
+
 const router = express.Router();
+
+// Configuración de multer para subida de archivos
+const upload = multer({ dest: 'uploads/' });
+
 router.route('/')
     .get(userController.getUsers)
-    .post(userController.createUser);
+    .post(upload.single('imageUrl'), userController.createUser);
+
 router.route('/:id')
-    .put(userController.updateUser)
+    .get(userController.getUserById)
+    .put(upload.single('imageUrl'), userController.updateUser)
     .delete(userController.deleteUser);
+
+// Ruta para obtener datos del usuario autenticado
+router.get("/me", authMiddleware, userController.getCurrentUser);
+
 export default router;
