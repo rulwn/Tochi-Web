03225f9df692d7945c90706f77da531a36f26a7a
LuiiiiDev <luisesc1210@gmail.com>
2025-06-18 21:45:55 -0600

    funciones de carrito arregladas

--- backend/src/routes/cartRoute.js
+++ backend/src/routes/cartRoute.js
@@ -1,10 +1,23 @@
 import express from 'express';
 import cartController from '../controllers/cartController.js';
+
 const router = express.Router();
+
 router.route('/')
-    .get(cartController.getCart)
-    .post(cartController.insertCart);
-router.route('/:id')
-    .put(cartController.updateCart)
-    .delete(cartController.deleteCart);
+    .get(cartController.getCart)        
+    .post(cartController.insertCart);   
+
+router.route('/user/:userId')
+    .get(cartController.getCartByUserId);
+
+router.route('/:userId')
+    .put(cartController.updateCart)     
+    .delete(cartController.deleteCart); 
+
+router.route('/:userId/confirm')
+    .put(cartController.confirmCart);   
+
+router.route('/:userId/cancel')
+    .put(cartController.cancelCart);    
+
 export default router;
